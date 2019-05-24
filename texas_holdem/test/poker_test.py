a23a18efce3b982189d34b7e498841ec12ff4d02
Juan Ortiz <jportiz32@gmail.com>
2019-05-24 15:23:03 -0400

    Update test case for poker.find_royal_flush

--- texas_holdem/test/poker_test.py
+++ texas_holdem/test/poker_test.py
@@ -4,25 +4,25 @@ from texas_holdem.main_game.poker import Poker, Card
 
 
 class PokerTest(unittest.TestCase):
-    def test_findRoyalFlush(self):
+    def test_find_royal_flush(self):
         poker = Poker()
 
         # best case scenario
         cards = [Card(1, 10), Card(1, 11), Card(
             1, 12), Card(1, 13), Card(1, 14)]
-        hasRoyalFlush = poker.find_flush_royal(cards)
+        hasRoyalFlush = poker.find_royal_flush(cards)
         self.assertTrue(hasRoyalFlush, "Cards should be a royal flush.")
 
         # test different suits, all high cards
         cards = [Card(2, 10), Card(1, 11), Card(
             1, 12), Card(1, 13), Card(1, 1)]
-        hasRoyalFlush = poker.find_flush_royal(cards)
+        hasRoyalFlush = poker.find_royal_flush(cards)
         self.assertFalse(
             hasRoyalFlush, "All cards should be of the same suit.")
 
         # test missing high card, same suits
         cards = [Card(1, 9), Card(1, 11), Card(1, 12), Card(1, 13), Card(1, 1)]
-        hasRoyalFlush = poker.find_flush_royal(cards)
+        hasRoyalFlush = poker.find_royal_flush(cards)
         self.assertFalse(
             hasRoyalFlush, "Should contain all high-cards[10,J,Q,K,A] cards.")
 
